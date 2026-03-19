df829902ab5e3dc7c89490997070377f09a77942
Gustavo Fernandes <gustavosergio2001@gmail.com>
2026-03-18 23:33:28 -0300

    Rework: Automatizar Inscrição, Isenção e Monitoramento

---
+++ doe/diario.py
@@ -0,0 +1,67 @@
+import os
+from datetime import datetime
+
+import pandas
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+
+import doe.telegram as telegram
+
+
+def init_driver():
+    if os.path.exists('/usr/bin/firefox'):
+        options = FirefoxOptions()
+        options.add_argument('-headless')
+        driver = webdriver.Firefox(options=options)
+        driver.implicitly_wait(10)
+    else:
+        options = ChromeOptions()
+        options.add_argument('-headless')
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(10)
+    return driver
+
+
+def processar_editais(driver, row):
+    driver.get('https://www.doe.sp.gov.br/busca-avancada')
+
+    input_pesquisa = '.css-brmobe > input:nth-child(1)'
+    txt_entrada = '.css-8atqhb > div:nth-child(3)'
+
+    driver.find_element(By.CSS_SELECTOR, input_pesquisa).send_keys(
+        f'{row['EDITAL']}')
+    webdriver.ActionChains(driver).send_keys(Keys.RETURN).perform()
+
+    if driver.find_elements(By.CSS_SELECTOR, txt_entrada):
+        abas_antes = driver.window_handles
+        driver.find_element(By.CSS_SELECTOR, txt_entrada).click()
+        nova_aba = list(set(driver.window_handles) - set(abas_antes))[0]
+        driver.switch_to.window(nova_aba)
+        return f'\n<code>{row['EDITAL']}</code> <code>({row['TIPO']})</code>  |  <a href="{driver.current_url}"><i>{row['MATERIA']}</i></a>'
+    else:
+        return ''
+
+
+def realizar_pesquisa(id):
+    driver = init_driver()
+    hoje = datetime.today().strftime('%d/%m/%Y')
+
+    menssagem = f'🎯 <b>EDITAIS COM ATUALIZAÇÃO EM {hoje}</b>'
+
+    editais = pandas.read_csv(
+        f'./doe/editais.csv',
+        delimiter=',',
+        dtype=str
+    )
+
+    for index, row in editais.iterrows():
+        menssagem += processar_editais(driver, row)
+
+    if menssagem.find('|') == -1:
+        menssagem = f'⏳ <b>EDITAIS SEM ATUALIZAÇÃO EM {hoje}</b>\n\n<i>Não houveram atualizações nos editais cadastrados :(</i>'
+
+    telegram.enviar_update(id, menssagem)
---
+++ inscricao/inscrever.py
@@ -0,0 +1,152 @@
+import os
+import time
+import logging
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
+
+
+from inscricao import candidato, formulario
+
+
+def init_driver():
+    if os.path.exists('/usr/bin/firefox'):
+        options = FirefoxOptions()
+        # options.add_argument('-headless')
+        driver = webdriver.Firefox(options=options)
+        driver.implicitly_wait(10)
+    else:
+        options = ChromeOptions()
+        # options.add_argument('-headless')
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(10)
+    return driver
+
+
+def css(driver, selector, wait=10, tentativas=3):
+    for tentativa in range(tentativas):
+        try:
+            return WebDriverWait(driver, wait).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, selector))
+            )
+        except StaleElementReferenceException:
+            if tentativa == tentativas - 1:
+                raise
+            time.sleep(0.5)
+
+
+def preencher(driver, selector, valor):
+    for tentativa in range(3):
+        try:
+            elemento = css(driver, selector)
+            elemento.send_keys(valor)
+            # time.sleep(0.5)
+            return
+        except StaleElementReferenceException:
+            if tentativa == 2:
+                raise
+            time.sleep(0.5)
+
+
+def clicar(driver, selector):
+    for tentativa in range(3):
+        try:
+            css(driver, selector).click()
+            # time.sleep(0.5)
+            return
+        except StaleElementReferenceException:
+            if tentativa == 2:
+                raise
+            time.sleep(0.5)
+
+
+def inscrever_pss(edital):
+    driver = init_driver()
+    dados = candidato.candidato
+    pss = formulario.pss
+
+    driver.get(
+        'https://urhsistemas.cps.sp.gov.br/dgsdad/selecaopublica/ETEC/PSS/Abertos.aspx'
+    )
+    wait = WebDriverWait(driver, 10)
+    time.sleep(1)
+    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+    # Filtro e seleção do edital
+    preencher(driver, pss.input_filtro, edital)
+    webdriver.ActionChains(driver).send_keys(Keys.RETURN).perform()
+    clicar(driver, pss.botao_filtro)
+
+    # ESC para fechar possível modal
+    time.sleep(1)
+    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+    clicar(driver, pss.botao_inscrever)
+
+    # CPF
+    preencher(driver, pss.input_cpf, dados.cpf)
+    webdriver.ActionChains(driver).send_keys(Keys.RETURN).perform()
+    time.sleep(1)
+    preencher(driver, pss.input_reptcpf, dados.cpf)
+    webdriver.ActionChains(driver).send_keys(Keys.RETURN).perform()
+
+    # Dados pessoais
+    preencher(driver, pss.input_nome, dados.nome)
+    clicar(driver, pss.radio_trans)
+    preencher(driver, pss.input_nasc, dados.nascimento)
+    preencher(driver, pss.input_rg, dados.rg)
+    clicar(driver, pss.radio_masc)
+
+    # Contato
+    preencher(driver, pss.input_email, dados.email)
+    preencher(driver, pss.input_altemail, dados.email)
+    preencher(driver, pss.input_ddd, dados.ddd)
+    preencher(driver, pss.input_fone, dados.fone)
+
+    # Estado civil (Select)
+    Select(css(driver, pss.input_civil)).select_by_index(1)
+
+    # Endereço
+    preencher(driver, pss.input_cep, dados.cep)
+    # time.sleep(2)  # aguarda autopreenchimento do CEP, se houver
+    preencher(driver, pss.input_rua, dados.rua)
+    preencher(driver, pss.input_numero, dados.numero)
+    preencher(driver, pss.input_bairro, dados.bairro)
+    preencher(driver, pss.input_cidade, dados.cidade)
+    preencher(driver, pss.input_estado, dados.estado)
+
+    # Informações adicionais
+    clicar(driver, pss.radio_jurado)
+    clicar(driver, pss.radio_cadunico)
+    clicar(driver, pss.radio_pcd)
+    clicar(driver, pss.botao_pessoal)
+
+    # Raça
+    Select(css(driver, pss.select_raca)).select_by_index(1)
+    clicar(driver, pss.botao_raca)
+
+    # Tipo de inscrição
+    Select(css(driver, pss.select_grad)).select_by_index(2)
+    clicar(driver, pss.botao_grad)
+
+    # Upload do memorial
+    css(driver, pss.input_memo).send_keys(os.path.abspath('./inscricao/Memorial.pdf'))
+    clicar(driver, pss.botao_memo)
+    time.sleep(3)
+
+    # Confirmações
+    clicar(driver, pss.check_1)
+    clicar(driver, pss.check_2)
+    clicar(driver, pss.check_3)
+    clicar(driver, pss.check_4)
+
+    # Submeter
+    clicar(driver, pss.botao_confirmar)
+    time.sleep(5)
+
+    driver.quit()
